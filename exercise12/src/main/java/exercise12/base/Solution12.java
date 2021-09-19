129fff9bbb44608abb42871e2d1cf46c24bf994b
Your0nlySon <eaduncan18@gmail.com>
2021-09-19 18:45:37 -0400

    updated psuedo

--- exercise12/src/main/java/exercise12/base/Solution12.java
+++ exercise12/src/main/java/exercise12/base/Solution12.java
@@ -14,34 +14,40 @@ public class Solution12 {
     public static void main(String[] args) {
         Solution12 app = new Solution12();
 
+        //Create methods and principal, rateOfInterest, numYears, and calcInvestment
         double principal = app.readPrincipleFromUser();
         double rateOfInterest = app.readROTFromUser();
         int numYears = app.readNumberOfYearsFromUser();
 
         double calcInvestment = app.calculateTheTotalInvestment(principal, rateOfInterest, numYears);
 
+        //print final statement
         System.out.println("After " + numYears + " years at " + rateOfInterest + "%, the investment will be worth $" + calcInvestment);
     }
 
     private double calculateTheTotalInvestment(double p, double rOI, int nY) {
+        //create double called totalInvest = A = P(1 + rt) and then round up to the nearest penny
         double totalInvest = (p * (1 + ((rOI / 100) * nY)));
         double tI = Math.ceil(totalInvest*100.0)/100.0;
         return tI;
     }
 
     private int readNumberOfYearsFromUser() {
+        //Read input for number of years
         System.out.println("Enter the number of years: ");
         int y = Integer.parseInt(in.nextLine());
         return y;
     }
 
     private double readROTFromUser() {
+        //Read the input for the rate of interest
         System.out.println("Enter the rate of interest: ");
         double r = Double.parseDouble(in.nextLine());
         return r;
     }
 
     private double readPrincipleFromUser() {
+        //Read the input from the user on the principle
         System.out.println("Enter the principle: ");
         double p = Double.parseDouble(in.nextLine());
         return p;
