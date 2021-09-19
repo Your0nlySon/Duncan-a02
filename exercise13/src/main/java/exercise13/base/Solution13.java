505986613bcae57cab57e6e56345a817f724018b
Your0nlySon <eaduncan18@gmail.com>
2021-09-19 18:51:23 -0400

    updated psuedo

--- exercise13/src/main/java/exercise13/base/Solution13.java
+++ exercise13/src/main/java/exercise13/base/Solution13.java
@@ -14,6 +14,7 @@ public class Solution13 {
     public static void main(String[] args) {
         Solution13 app = new Solution13();
 
+        //create and call methods called principle, rateOfInterest, numYears, compoundYears, and calcInvestment
         double principal = app.readPrincipleFromUser();
         double rateOfInterest = app.readROTFromUser();
         int numYears = app.readNumberOfYearsFromUser();
@@ -25,30 +26,35 @@ public class Solution13 {
     }
 
     private int readCompoundYearsFromUser() {
+        //Read input from user for Compound Years
         System.out.println("What is the number of times the interest is compounded per year? ");
         int c = Integer.parseInt(in.nextLine());
         return c;
     }
 
     private double calculateTheTotalInvestment(double p, double rOI, int nY, int cY) {
+        //calculate totalInvest = A = P(1 + r/n)^(n*t)
         double totalInvest = (p * Math.pow(1 + ((rOI / 100) / cY), (cY * nY)));
         double tI = Math.ceil(totalInvest*100.0)/100.0;
         return tI;
     }
 
     private int readNumberOfYearsFromUser() {
+        //Read input from user for Number of Years
         System.out.println("Enter the number of years: ");
         int y = Integer.parseInt(in.nextLine());
         return y;
     }
 
     private double readROTFromUser() {
+        //Read input from user for Rate of interest
         System.out.println("Enter the rate of interest: ");
         double r = Double.parseDouble(in.nextLine());
         return r;
     }
 
     private double readPrincipleFromUser() {
+        //Read input from user for Principle
         System.out.println("Enter the principle: ");
         double p = Double.parseDouble(in.nextLine());
         return p;
